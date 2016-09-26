8600b4bb05941ce39a67553657ff64d6712e58ef
MejiUlises <jonhylargo@hotmail.com>
2016-09-26 18:19:50 -0500

    Added passport skeleton. Added method to register and authenticate users

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const multer = require('multer');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+
 var upload = multer({
     dest: 'uploads/'
 });
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const app = express();
+var passport = require('passport');
 var db
 
 
@@ -26,10 +31,101 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json());
 
+// Logging
+//app.use(morgan('dev'));
+
+// Use the passport package in our application
+app.use(passport.initialize());
+
 // Send HTML file
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html')
 })
+
+// Register new user
+app.post('/apiv1/register', function(req, res) {
+    db.collection('users').findOne({
+        email: req.body.email
+    }, function(err, results) {
+        if (err) console.log(err);
+        if (results) {
+            console.log("Existe el email");
+            res.end();
+        } else {
+            console.log("No existe el email");
+            bcrypt.hash(req.body.password, 10, function(err, hash) {
+                if (err) console.log(err);
+
+                db.collection('users').save({
+                    email: req.body.email,
+                    password: hash
+                }, function(err, results) {
+                    if (err) console.log(err);
+                    return res.status(201).json(results);
+                })
+            })
+        }
+    })
+})
+
+
+app.post('/apiv1/authenticate', function(req, res) {
+    //console.log(req);
+    db.collection('users').findOne({
+        email: req.body.email
+    }, function(err, results) {
+        if (err) {
+            res.status(500);
+            console.log("ERROR " + err);
+        }
+        if (results) {
+            console.log('Body Passworld: ' + req.body.password);
+            console.log(results.password);
+            bcrypt.compare(req.body.password, results.password, function(err, isMatch) {
+                    if (err) console.log(err);
+                    if (isMatch) {
+                        var token = jwt.sign(req.body, 'hotCake', {
+                            expiresIn: 3600 // in seconds
+                        });
+                        console.log("POST / authenticate")
+                        results.token = token;
+                        res.status(200).json(results);
+                    } else {
+                        console.log("Wrong Password")
+                        res.status(403).json(req.body);
+                    }
+
+                })
+                /*
+                  console.log(results)
+                  if (req.body.password === results.password) {
+                      var token = jwt.sign(req.body, 'hotCake', {
+                          expiresIn: 3600 // in seconds
+                      });
+                      console.log(token);
+                      console.log("POST / authenticate")
+                      res.status(200).json(results);
+                  } else {
+                      console.log("Wrong Password")
+                      res.status(403).json(req.body);
+                  }
+                  */
+        } else {
+            console.log("User NOT FOUND");
+            res.status(404).json(req.body);
+        }
+    })
+})
+
+app.get('/apiv1/nomamar', passport.authenticate('jwt', {
+        session: false
+    }),
+    function(req, res) {
+        console.log(req);
+        console.log(res);
+        res.send("Succsssss");
+    }
+);
 
 // Get the list of all acuerdos
 app.get('/apiv1/acuerdos', function(req, res) {
